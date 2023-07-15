bc9808f83e4c0071f4faa103c19e650009422c8e
shaoyuancc <shaoyuan.chewchia@letsbramble.com>
2023-07-15 15:44:13 -0400

    trichal3 solved

--- large_gcs/graph/contact_cost_constraint_factory.py
+++ large_gcs/graph/contact_cost_constraint_factory.py
@@ -84,6 +84,44 @@ def contact_shortcut_edge_cost_factory_over(
     return costs
 
 
+def contact_shortcut_edge_cost_factory_over_obj_weighted(
+    u_vars: ContactSetDecisionVariables, v_vars: ContactSetDecisionVariables
+) -> List[Cost]:
+    """Creates a list of costs for the shortcut between set u and set v"""
+    u_vars_all = create_vars_from_template(u_vars.all, "u")
+    v_vars_all = create_vars_from_template(v_vars.all, "v")
+    # Hacky way to separate the object and robot positions variables
+    # I know that v will be the target, and so will not have force variables
+    # I know that only robots have actuation force variables
+    n_robs = u_vars.force_act.shape[0]
+    n_objs = u_vars.pos.shape[0] - n_robs
+    # Position continuity cost
+    u_pos = u_vars.pos_from_all(u_vars_all)
+    v_pos = v_vars.pos_from_all(v_vars_all)
+    uv_vars_all = np.concatenate((u_vars_all, v_vars_all))
+
+    def create_quadratic_cost(u_pos, v_pos, scaling=1):
+        u_last_pos = u_pos[:, :, -1].flatten()
+        v_first_pos = v_pos[:, :, 0].flatten()
+        diff = (u_last_pos - v_first_pos).flatten()
+        expr = np.dot(diff, diff) * scaling
+        var_map = {var.get_id(): i for i, var in enumerate(uv_vars_all)}
+        Q, b, c = DecomposeQuadraticPolynomial(Polynomial(expr), var_map)
+        return QuadraticCost(Q, b, c)
+
+    costs = [
+        create_quadratic_cost(u_pos[:n_objs], v_pos[:n_objs], scaling=10),
+        create_quadratic_cost(u_pos[n_objs:], v_pos[n_objs:], scaling=1),
+    ]
+
+    # Constant cost for the edge
+    a = np.zeros((uv_vars_all.size, 1))
+    constant_cost = 1
+    costs.append(LinearCost(a, constant_cost))
+
+    return costs
+
+
 ### VERTEX COST CREATION ###
 
 
--- large_gcs/scripts/contact_astar_demo.py
+++ large_gcs/scripts/contact_astar_demo.py
@@ -7,6 +7,7 @@ from large_gcs.algorithms.gcs_astar import GcsAstar
 from large_gcs.algorithms.gcs_astar_subopt import GcsAstarSubOpt
 from large_gcs.graph.contact_cost_constraint_factory import (
     contact_shortcut_edge_cost_factory_over,
+    contact_shortcut_edge_cost_factory_over_obj_weighted,
 )
 import random
 import matplotlib.pyplot as plt
@@ -14,10 +15,16 @@ import matplotlib.cm as cm
 import os
 
 np.set_printoptions(formatter={"float": lambda x: "{0:0.3f}".format(x)})
-base = "contact_graph_triangle_challenge_full"
+base_filename = "contact_graph_triangle_challenge_full"
+base_filename = "cg_trichal2_full"
+base_filename = "cg_trichal3_full"
+
+
 method_modifier = "gcs_astar_subopt_shortestedges"
+method_modifier = "gcs_astar_subopt_shortestedges_obj_weighted"
+
 graph_file = os.path.join(
-    os.environ["PROJECT_ROOT"], "large_gcs", "example_graphs", base + ".npy"
+    os.environ["PROJECT_ROOT"], "large_gcs", "example_graphs", base_filename + ".npy"
 )
 cg = ContactGraph.load_from_file(graph_file)
 print(cg.params)
@@ -25,14 +32,14 @@ print(cg.params)
 gcs_astar = GcsAstarSubOpt(
     cg,
     use_convex_relaxation=False,
-    shortcut_edge_cost_factory=contact_shortcut_edge_cost_factory_over,
+    shortcut_edge_cost_factory=contact_shortcut_edge_cost_factory_over_obj_weighted,
 )
 sol = gcs_astar.run(verbose=True, animate=False)
 
 cg._post_solve(sol)
 output_dir = os.path.join(os.environ["PROJECT_ROOT"], "output", "contact")
-vid_file = os.path.join(output_dir, f"{method_modifier}_{base}.mp4")
-graphviz_file = os.path.join(output_dir, f"{method_modifier}_{base}")
+vid_file = os.path.join(output_dir, f"{method_modifier}_{base_filename}.mp4")
+graphviz_file = os.path.join(output_dir, f"{method_modifier}_{base_filename}")
 gviz = gcs_astar._visited.graphviz()
 gviz.format = "pdf"
 gviz.render(graphviz_file, view=False)
