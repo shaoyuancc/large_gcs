b5a98f047ba5793f82260ee3368f11eada747224
Shao Yuan <flossy_lineage.0b@icloud.com>
2023-07-21 16:22:27 -0400

    manual maze a2 soln

--- large_gcs/graph/contact_graph.py
+++ large_gcs/graph/contact_graph.py
@@ -1,3 +1,4 @@
+import logging
 from collections import defaultdict
 from dataclasses import dataclass
 from itertools import combinations, product
@@ -24,6 +25,8 @@ from large_gcs.graph.contact_cost_constraint_factory import (
     vertex_cost_position_path_length,
 )
 from large_gcs.graph.graph import Graph, ShortestPathSolution
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -115,12 +118,10 @@ class ContactGraph(Graph):
         )
         self.set_source("source")
         self.set_target("target")
-
         if edge_keys is None:
             edges = self._generate_contact_graph_edges(set_ids)
         else:
-            edges = edge_keys
-
+            edges = self._filter_edge_keys(edge_keys)
         self.add_edges_from_vertex_names(
             *zip(*edges),
             costs=self._create_edge_costs(edges),
@@ -128,17 +129,16 @@ class ContactGraph(Graph):
         )
 
         # Check that the source and target are reachable
-        assert (
-            len(self.outgoing_edges(self.source_name)) > 0
-        ), "Source does not overlap with any other set"
-        assert (
-            len(self.incoming_edges(self.target_name)) > 0
-        ), "Target is not reachable from any other set"
+        if len(self.outgoing_edges(self.source_name)) == 0:
+            logger.warn("Source does not overlap with any other set")
+        if len(self.incoming_edges(self.target_name)) == 0:
+            logger.warn("Target is not reachable from any other set")
 
-        print(f"Created contact graph: {self.params}")
+        logger.info(f"Created contact graph: {self.params}")
 
     ### VERTEX AND EDGE COSTS AND CONSTRAINTS ###
     def _create_vertex_costs(self, sets: List[ContactSet]) -> List[List[Cost]]:
+        logger.info("Creating vertex costs...")
         costs = [
             [
                 vertex_cost_position_path_length(set.vars),
@@ -146,7 +146,7 @@ class ContactGraph(Graph):
             ]
             if not isinstance(set, ContactPointSet)
             else []
-            for set in sets
+            for set in tqdm(sets)
         ]
         return costs
 
@@ -156,20 +156,22 @@ class ContactGraph(Graph):
         return [[] for set in sets]
 
     def _create_edge_costs(self, edges: List[Tuple[str, str]]) -> List[List[Cost]]:
+        logger.info("Creating edge costs...")
         return [
             [
                 edge_cost_constant(
                     self.vertices[u].convex_set.vars, self.vertices[v].convex_set.vars
                 )
             ]
-            for u, v in edges
+            for u, v in tqdm(edges)
         ]
 
     def _create_edge_constraints(
         self, edges: List[Tuple[str, str]]
     ) -> List[List[Constraint]]:
+        logger.info("Creating edge constraints...")
         constraints = []
-        for u, v in edges:
+        for u, v in tqdm(edges):
             constraints.append(
                 [
                     edge_constraint_position_continuity(
@@ -186,7 +188,7 @@ class ContactGraph(Graph):
         self, contact_set_ids: List[str]
     ) -> List[Tuple[str, str]]:
         """Generates all possible edges given a set of contact sets."""
-        print("Generating edges...(parallel)")
+        logger.info("Generating edges...(parallel)")
         with Pool() as pool:
             pairs = list(combinations(contact_set_ids, 2))
             sets = [
@@ -204,13 +206,20 @@ class ContactGraph(Graph):
                 if intersect:
                     edges.append((u, v))
                     edges.append((v, u))
-        print(f"{len(edges)} edges generated")
+        logger.info(f"{len(edges)} edges generated")
         return edges
 
     @staticmethod
     def _check_intersection(args):
         u_set, v_set = args
         return u_set.IntersectsWith(v_set)
+
+    def _filter_edge_keys(self, edge_keys: List[Tuple[str, str]]):
+        edges = []
+        for u, v in edge_keys:
+            if u in self.vertices and v in self.vertices:
+                edges.append((u, v))
+        return edges
 
     def _generate_contact_sets(
         self,
@@ -235,12 +244,12 @@ class ContactGraph(Graph):
         movable = obj_names + rob_names
 
         if contact_pair_modes is None or contact_set_mode_ids is None:
-            print(f"Generating contact sets for {len(body_dict)} bodies...")
+            logger.info(f"Generating contact sets for {len(body_dict)} bodies...")
             static_movable_pairs = list(product(obs_names, movable))
             movable_pairs = list(combinations(movable, 2))
             rigid_body_pairs = static_movable_pairs + movable_pairs
 
-            print(
+            logger.info(
                 f"Generating contact pair modes for {len(rigid_body_pairs)} body pairs..."
             )
 
@@ -250,7 +259,7 @@ class ContactGraph(Graph):
                 )
                 for body1, body2 in tqdm(rigid_body_pairs)
             }
-            print(
+            logger.info(
                 f"Each body pair has on average {np.mean([len(modes) for modes in body_pair_to_modes.values()])} modes"
             )
             body_pair_to_mode_names = {
@@ -265,7 +274,7 @@ class ContactGraph(Graph):
             # Each set is the cartesian product of the modes for each object pair
             set_ids = list(product(*body_pair_to_mode_names.values()))
         else:
-            print(
+            logger.info(
                 f"Loading {len(contact_pair_modes)} contact pair modes for {len(body_dict)} bodies..."
             )
             mode_ids_to_mode = contact_pair_modes
@@ -302,7 +311,7 @@ class ContactGraph(Graph):
                     eq(body.vars_force_res, body_force_sums[body_name]).tolist()
                 )
 
-        print(f"Generating contact sets for {len(set_ids)} sets...")
+        logger.info(f"Generating contact sets for {len(set_ids)} sets...")
 
         all_contact_sets = [
             ContactSet(
@@ -315,21 +324,21 @@ class ContactGraph(Graph):
         ]
         # Do not need to prune if loading from saved file, there shouldn't be empty sets
         if contact_set_mode_ids is None:
-            print(f"Pruning empty sets...")
+            logger.info(f"Pruning empty sets...")
             sets_to_keep = [
                 contact_set
                 for contact_set in tqdm(all_contact_sets)
                 if not contact_set.set.IsEmpty()
             ]
 
-            print(
+            logger.info(
                 f"{len(sets_to_keep)} sets remain after removing {len(all_contact_sets) - len(sets_to_keep)} empty sets"
             )
         else:
             sets_to_keep = all_contact_sets
 
         if vertex_exlcusion is not None:
-            print(f"Removing sets matching exclusion strings {vertex_exlcusion}")
+            logger.info(f"Removing sets matching exclusion strings {vertex_exlcusion}")
             sets_to_keep = [
                 contact_set
                 for contact_set in tqdm(sets_to_keep)
@@ -338,10 +347,10 @@ class ContactGraph(Graph):
                     for vertex_exlcusion in vertex_exlcusion
                 )
             ]
-            print(f"{len(sets_to_keep)} sets remain after removing excluded sets")
+            logger.info(f"{len(sets_to_keep)} sets remain after removing excluded sets")
 
         if vertex_inclusion is not None:
-            print(f"Filtering sets for inclusion strings {vertex_inclusion}")
+            logger.info(f"Filtering sets for inclusion strings {vertex_inclusion}")
             sets_to_keep = [
                 contact_set
                 for contact_set in tqdm(sets_to_keep)
@@ -350,7 +359,9 @@ class ContactGraph(Graph):
                     for vertex_inclusion in vertex_inclusion
                 )
             ]
-            print(f"{len(sets_to_keep)} sets remain after filtering for inclusion sets")
+            logger.info(
+                f"{len(sets_to_keep)} sets remain after filtering for inclusion sets"
+            )
 
         sets_to_keep_ids = [str(contact_set.id) for contact_set in sets_to_keep]
         return sets_to_keep, sets_to_keep_ids
@@ -359,10 +370,12 @@ class ContactGraph(Graph):
 
     def _post_solve(self, sol: ShortestPathSolution):
         """Post solve hook that is called after solving by the base graph class"""
-
-        self.contact_spp_sol = self.create_contact_spp_sol(
-            sol.vertex_path, sol.ambient_path
-        )
+        if sol.is_success:
+            self.contact_spp_sol = self.create_contact_spp_sol(
+                sol.vertex_path, sol.ambient_path
+            )
+        else:
+            logger.warn("No Shortest Path Solution Found")
 
     def create_contact_spp_sol(self, vertex_path, ambient_path):
         """An ambient path is a list of vertices in the higher dimensional space"""
@@ -386,6 +399,23 @@ class ContactGraph(Graph):
 
     ### PLOTTING AND ANIMATING ###
 
+    def plot(self):
+        for body in self.obstacles:
+            body.plot()
+        for body, pos in zip(self.objects, self.source_pos[: self.n_objects]):
+            body.plot_at_position(
+                pos=pos, label_vertices_faces=True, color=BodyColor["object"]
+            )
+        for body, pos in zip(self.robots, self.source_pos[self.n_objects :]):
+            body.plot_at_position(
+                pos=pos, label_vertices_faces=True, color=BodyColor["robot"]
+            )
+        for body, pos in zip(
+            self.objects + self.robots,
+            self.target_pos,
+        ):
+            body.plot_at_position(pos=pos, color=BodyColor["target"])
+
     def plot_samples_in_set(self, set_name: str, n_samples: int = 100, **kwargs):
         """Plots a single set"""
         options = {"facecolor": "mintcream", "edgecolor": "k", "zorder": 1}
@@ -393,7 +423,7 @@ class ContactGraph(Graph):
         plt.axis("equal")
         vertex = self.vertices[set_name]
         samples = vertex.convex_set.get_samples(n_samples)
-        # print(samples)
+        # logger.info(samples)
         raise NotImplementedError
         # Need to modify decompose_ambient_path
         obj_pos_trajectories, rob_pos_trajectories = self.decompose_ambient_path(
@@ -404,10 +434,10 @@ class ContactGraph(Graph):
             # Plot object trajectories
             for i in range(obj_pos_trajectories.shape[0]):
                 self.objects[i].plot_at_position(obj_pos_trajectories[i, j])
-                # print(f"obj_pos: {obj_pos_trajectories[i, j]}")
+                # logger.info(f"obj_pos: {obj_pos_trajectories[i, j]}")
             for i in range(rob_pos_trajectories.shape[0]):
                 self.robots[i].plot_at_position(rob_pos_trajectories[i, j])
-                # print(f"rob_pos: {rob_pos_trajectories[i, j]}")
+                # logger.info(f"rob_pos: {rob_pos_trajectories[i, j]}")
         for obs in self.obstacles:
             obs.plot()
 
@@ -444,6 +474,8 @@ class ContactGraph(Graph):
         plt.show()
 
     def animate_solution(self):
+        import textwrap
+
         import matplotlib.animation as animation
         import matplotlib.patches as patches
 
@@ -481,13 +513,32 @@ class ContactGraph(Graph):
             ax.text(*body.geometry.center, label, ha="center", va="center")
             for body, label in zip(bodies, label_text)
         ]
-        vertex_annotation = ax.annotate(
-            transition_map[0],
-            xy=(0.95, 0.95),
-            xycoords="axes fraction",
-            ha="right",
-            va="top",
+        # Wrap the text to fit within the width of the plot
+        text_wrap_width = 80
+        wrapped_text = "\n".join(
+            textwrap.wrap(
+                self.contact_spp_sol.vertex_path[transition_map[0]],
+                width=text_wrap_width,
+                break_long_words=False,
+                break_on_hyphens=False,
+            )
         )
+
+        # Adjust the position of the annotation
+        vertex_annotation = ax.annotate(
+            wrapped_text,
+            xy=(0.5, 1),
+            xycoords="axes fraction",
+            xytext=(0, 15),
+            textcoords="offset points",
+            ha="center",
+            va="bottom",
+            bbox=dict(boxstyle="round", fc="w"),
+        )
+        plt.subplots_adjust(
+            top=0.85
+        )  # Adjust subplot parameters to give the annotation enough space
+
         for poly in polygons:
             ax.add_patch(poly)
 
@@ -506,11 +557,15 @@ class ContactGraph(Graph):
                 if i in transition_map:
                     # force_res_quivers[j].set_offsets(poly_offset[j] + trajs[i][j])
                     # force_res_quivers[j].set_UVC(*force_res_vals[j][transition_map[i]])
-                    vertex_annotation.set_text(
-                        self.contact_spp_sol.vertex_path[transition_map[i]]
+                    wrapped_text = "\n".join(
+                        textwrap.wrap(
+                            self.contact_spp_sol.vertex_path[transition_map[i]],
+                            width=text_wrap_width,
+                            break_long_words=False,
+                            break_on_hyphens=False,
+                        )
                     )
-                else:
-                    pass
+                    vertex_annotation.set_text(wrapped_text)
                     # force_res_quivers[j].set_offsets(poly_offset[j] + trajs[i][j])
             return polygons
 
@@ -526,7 +581,7 @@ class ContactGraph(Graph):
         # Input has shape (n_movable bodies, n_sets_in_path, n_pos_per_set, n_base_dim) OLD
         # Input has shape (n_pos, n_bodies, n_base_dim)
         trajs_in = contact_sol.pos_trajs
-        # print(f"trajs_in shape {trajs_in.shape}")
+        # logger.info(f"trajs_in shape {trajs_in.shape}")
         transition_map = {}
         # Final list is going to have shape (n_pos, n_movable bodies (objects then robots), n_base_dim)
         trajs_out = []
@@ -584,7 +639,12 @@ class ContactGraph(Graph):
         )
 
     @classmethod
-    def load_from_file(cls, path: str):
+    def load_from_file(
+        cls,
+        path: str,
+        vertex_inclusion: List[str] = None,
+        vertex_exclusion: List[str] = None,
+    ):
         data = np.load(path, allow_pickle=True).item()
         obs = [RigidBody.from_params(params) for params in data["obs_params"]]
         objs = [RigidBody.from_params(params) for params in data["objs_params"]]
@@ -601,6 +661,11 @@ class ContactGraph(Graph):
                 params.contact_location_b_type(body_b, params.contact_location_b_index),
             )
             contact_pair_modes[mode.id] = mode
+        if vertex_inclusion is None:
+            vertex_inclusion = data["vertex_inclusion"]
+        if vertex_exclusion is None:
+            vertex_exclusion = data["vertex_exclusion"]
+
         cg = cls(
             obs,
             objs,
@@ -610,8 +675,8 @@ class ContactGraph(Graph):
             data["target_pos_objs"],
             data["target_pos_robs"],
             data["workspace"],
-            data["vertex_exclusion"],
-            data["vertex_inclusion"],
+            vertex_exclusion,
+            vertex_inclusion,
             contact_pair_modes,
             data["contact_set_mode_ids"],
             data["edge_keys"],
