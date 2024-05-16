49f0ffa0478eea7e50738124dafa8b20cbf5ee74
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-05-16 15:01:08 +0200

    Add support for AH-comparison

--- large_gcs/visualize/plot_sampling_comparison.py
+++ large_gcs/visualize/plot_sampling_comparison.py
@@ -10,21 +10,28 @@ from large_gcs.graph.graph import ShortestPathSolution
 from large_gcs.utils.utils import use_type_1_fonts_in_plots
 
 
-class SingleSamplingRunData(NamedTuple):
-    num_samples: int
+class SingleRunData(NamedTuple):
     cost: float
     wall_clock_time: float
     num_paths_expanded: int
 
 
+class SamplingSingleRunData(NamedTuple):
+    cost: float
+    wall_clock_time: float
+    num_paths_expanded: int
+    num_samples: int
+
+
 class SamplingRunData:
-    def __init__(self, run_data: List[SingleSamplingRunData]) -> None:
+    def __init__(self, run_data: List[SamplingSingleRunData | SingleRunData]) -> None:
         self.data = run_data
 
     @classmethod
     def load_from_folder(cls, data_dir: Path) -> "SamplingRunData":
         run_data = []
 
+        all_runs_are_sampled = True
         # Iterate through the runs and generate figures
         for path in data_dir.iterdir():
             # We only care about the subfolders (which contain data for a run)
@@ -40,7 +47,7 @@ class SamplingRunData:
                     "does not exist."
                 )
 
-            cfg: DictConfig = OmegaConf.load(config_path)
+            cfg: DictConfig = OmegaConf.load(config_path)  # type: ignore
 
             sol_files = list(path.glob("*_solution.pkl"))
             if not len(sol_files) == 1:
@@ -60,21 +67,45 @@ class SamplingRunData:
             metric_file = metric_files[0]
             metrics = AlgMetrics.load(metric_file)
 
-            num_samples = cfg.domination_checker.num_samples_per_vertex
-
-            data = SingleSamplingRunData(
-                num_samples, sol.cost, metrics.time_wall_clock, 0
+            # for hierarchical methods, we will have more entries.
+            # For this method, we only have one.
+            HIERARCHY_IDX = str(0)
+            n_paths_expanded = (
+                metrics.n_vertices_expanded[HIERARCHY_IDX]  # type: ignore
+                + metrics.n_vertices_reexpanded[HIERARCHY_IDX]  # type: ignore
             )
+            if "num_samples_per_vertex" in cfg.domination_checker.keys():
+                num_samples = cfg.domination_checker.num_samples_per_vertex
+
+                data = SamplingSingleRunData(
+                    sol.cost,
+                    metrics.time_wall_clock,
+                    n_paths_expanded,
+                    num_samples,
+                )
+            else:
+                data = SingleRunData(
+                    sol.cost,
+                    metrics.time_wall_clock,
+                    n_paths_expanded,
+                )
+                all_runs_are_sampled = False
+
             run_data.append(data)
 
-        # sort data based on num_samples
-        run_data = sorted(run_data, key=lambda d: d.num_samples)
+        if all_runs_are_sampled:
+            # sort data based on num_samples
+            run_data = sorted(run_data, key=lambda d: d.num_samples)
 
         return cls(run_data)
 
     @cached_property
     def num_samples(self) -> List[int]:
-        return [d.num_samples for d in self.data]
+        if not all([type(d) == SamplingSingleRunData for d in self.data]):
+            raise RuntimeError(
+                "Can only compute number of samples when all data points are from sampling runs"
+            )
+        return [d.num_samples for d in self.data]  # type: ignore
 
     @cached_property
     def solve_times(self) -> List[float]:
@@ -88,7 +119,25 @@ class SamplingRunData:
     def num_paths_expanded(self) -> List[int]:
         return [d.num_paths_expanded for d in self.data]
 
-    def make_plot(self, output_path: Optional[Path] = None) -> None:
+    def save(
+        self,
+        output_path: Path,
+    ) -> None:
+        """
+        Saves all the datapoints in this run to a JSON file.
+        """
+        # Convert to dictionary and save to JSON
+        data_dict_list = [data._asdict() for data in self.data]
+        import json
+
+        with open(output_path, "w") as f:
+            json.dump(data_dict_list, f, indent=4)
+
+    def make_plot(
+        self,
+        ah_baseline: Optional[SingleRunData] = None,
+        output_path: Optional[Path] = None,
+    ) -> None:
         use_type_1_fonts_in_plots()
 
         fig_height = 4
@@ -108,6 +157,11 @@ class SamplingRunData:
 
         axs[2].plot(self.num_samples, self.costs)
         axs[2].set_title("Cost")
+
+        if ah_baseline:
+            axs[0].axhline(ah_baseline.wall_clock_time)
+            axs[1].axhline(ah_baseline.num_paths_expanded)
+            axs[2].axhline(ah_baseline.cost)
 
         if output_path:
             fig.savefig(output_path)
--- scripts/plot_generation/generate_comparison_plot.py
+++ scripts/plot_generation/generate_comparison_plot.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
 
-from large_gcs.visualize.plot_sampling_comparison import SamplingRunData
+from large_gcs.visualize.plot_sampling_comparison import SamplingRunData, SingleRunData
 
 
 def main() -> None:
@@ -12,12 +12,18 @@ def main() -> None:
         "--dir",
         type=str,
         help="Path to the directory containing data and config file",
+        required=True,
+    )
+    parser.add_argument(
+        "--ah_dir",
+        type=str,
+        help="Path to the directory containing data and config file for the AH-containment comparison",
+        default=None,
     )
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Get the directory path
     data_dir = Path(args.dir)
 
     # Validate the directory
@@ -27,7 +33,18 @@ def main() -> None:
 
     run_data = SamplingRunData.load_from_folder(data_dir)
 
-    run_data.make_plot()
+    if args.ah_dir is not None:
+        ah_comparison_data_dir = Path(args.ah_dir)
+        ah_run_data = SamplingRunData.load_from_folder(ah_comparison_data_dir)
+        ah_run_data.save(ah_comparison_data_dir / "aggregated_run_data.json")
+
+        assert len(ah_run_data.data) == 1
+        assert type(ah_run_data.data[0]) is SingleRunData
+        ah_data = ah_run_data.data[0]
+
+        run_data.make_plot(ah_data)
+    else:
+        run_data.make_plot()
 
 
 if __name__ == "__main__":
