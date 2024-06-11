02c22201407b3ed5c25eea5c53939a257490e293
rhjiang <rhjiang@mit.edu>
2024-06-11 10:39:17 -0400

    rerun all experiments with 10h timeout

--- scripts/run_experiment_w_timeout.sh
+++ scripts/run_experiment_w_timeout.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-TIMEOUT_DURATION=10s
+TIMEOUT_DURATION=10h
 
 # Note: on Mac you'll need to install coreutils to get timeout `brew install coreutils`
-# Run the Python script with a timeout of 60 seconds
-timeout $TIMEOUT_DURATION python3 ../experiments/run_contact_graph_experiment.py --config-name cg_gcs_astar_conv_res.yaml
+
+# Run multiple experiments with different configurations
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_new_sampling
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_eps_suboptimal_sampling
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_optimal_sampling
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_new_containment
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_optimal_containment
+# timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_eps_suboptimal_containment
+# Baseline method
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name ixg_baseline
+timeout $TIMEOUT_DURATION python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name ixg_star_eps_suboptimal_baseline
+# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name ixg_star_optimal_baseline
 
 # Check the exit status
 if [ $? -eq 124 ]; then
--- scripts/run_multiple_experiments.sh
+++ scripts/run_multiple_experiments.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Run multiple experiments with different configurations
-# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_new_sampling
-# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_eps_suboptimal_sampling
-# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_optimal_sampling
+python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_new_sampling
+python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_eps_suboptimal_sampling
+python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_optimal_sampling
 python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_new_containment
-python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_eps_suboptimal_containment
-# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_optimal_containment
+python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_optimal_containment
+# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name reaches_cheaper_eps_suboptimal_containment
+# Baseline method
+python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name ixg_baseline
+python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name ixg_star_eps_suboptimal_baseline
+# python3 experiments/run_contact_graph_experiment.py --config-path ../config/WAFR_experiments --config-dir config --config-name ixg_star_optimal_baseline
 
+
+
+# Full Graph Creation
 # python3 scripts/full_contact_graph_generator.py cg_simple_3
 # python3 scripts/full_contact_graph_generator.py cg_trichal4
 # python3 scripts/full_contact_graph_generator.py cg_maze_b1
