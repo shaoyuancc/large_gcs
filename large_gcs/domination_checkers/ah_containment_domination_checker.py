204f5b8d63cba50fcc6c71abda49bccd5d310118
Shao Yuan <flossy_lineage.0b@icloud.com>
2024-06-27 19:06:56 -0400

    Named vertex and edge variables by index to allow for cycles in ah_containment_domination_checker.py

--- large_gcs/domination_checkers/ah_containment_domination_checker.py
+++ large_gcs/domination_checkers/ah_containment_domination_checker.py
@@ -5,13 +5,11 @@ import numpy as np
 import pypolycontain as pp
 import scipy
 from pydrake.all import (
-    Constraint,
     GurobiSolver,
     HPolyhedron,
     L1NormCost,
     LinearConstraint,
     LinearCost,
-    LinearEqualityConstraint,
     MathematicalProgram,
     MathematicalProgramResult,
     SolverOptions,
@@ -296,207 +294,6 @@ class AHContainmentDominationChecker(DominationChecker):
         # logger.debug(f"Solver name: {result.get_solver_id().name()}")
         return result.is_success()
 
-    def get_path_A_b_C_d(
-        self, node: SearchNode
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-        """Get the A, b, C, d matrices that define the polyhedron that
-        represents the path.
-
-        Where Ax <= b are the inequality constraints and Cx = d are the
-        equality constraints. A.shape = (m, N) where m is the number of
-        constraints and N is the total number of decision variables.
-        b.shape = (m,) C.shape = (p, N) where p is the number of
-        equality constraints d.shape = (p,)
-        """
-        # ASSUMPTION: polyhedral sets for the vertices. Linear costs for the vertices and edges.
-
-        # First, collect all the decision variables
-        vertices = [self._graph.vertices[name].gcs_vertex for name in node.vertex_path]
-        edges = [self._graph.edges[edge].gcs_edge for edge in node.edge_path]
-
-        v_dims = [v.ambient_dimension() for v in vertices]
-        current_index = 0
-        # Collect the indices of the decision variables for each vertex
-        x = []
-        for i, dim in enumerate(v_dims):
-            x.append(list(range(current_index, current_index + dim)))
-            current_index += dim
-        # Total number of decision variables
-        N = current_index
-
-        if self.include_cost_epigraph:
-            # Use a mathematical prog to collate the costs
-            prog = MathematicalProgram()
-            vertex_vars = [
-                prog.NewContinuousVariables(
-                    v.ambient_dimension(), name=f"{v_name}_vars"
-                )
-                for v, v_name in zip(vertices, node.vertex_path)
-            ]
-            # Vertex Costs:
-            for v, v_name, v_vars in zip(vertices, node.vertex_path, vertex_vars):
-                for binding in v.GetCosts():
-                    cost = binding.evaluator()
-                    if isinstance(cost, L1NormCost):
-                        A = cost.A()
-                        t = prog.NewContinuousVariables(
-                            A.shape[0], name=f"{v_name}_vertex_l1norm_cost"
-                        )
-                        prog.AddLinearCost(np.ones(t.shape), t)
-                        prog.AddLinearConstraint(
-                            A @ v_vars - t,
-                            np.ones(A.shape[0]) * (-np.inf),
-                            np.zeros(A.shape[0]),
-                        )
-                        prog.AddLinearConstraint(
-                            -A @ v_vars - t,
-                            np.ones(A.shape[0]) * (-np.inf),
-                            np.zeros(A.shape[0]),
-                        )
-                    else:
-                        prog.AddCost(cost, v_vars)
-            # Edge Costs:
-            for e, e_name in zip(edges, node.edge_path):
-                edge = self._graph.edges[e_name]
-                u_name, v_name = edge.u, edge.v
-                u_idx, v_idx = node.vertex_path.index(u_name), node.vertex_path.index(
-                    v_name
-                )
-                for binding in e.GetCosts():
-                    cost = binding.evaluator()
-                    e_vars = np.concatenate((vertex_vars[u_idx], vertex_vars[v_idx]))
-                    if isinstance(cost, L1NormCost):
-                        A = cost.A()
-                        t = prog.NewContinuousVariables(
-                            A.shape[0], name=f"{e_name}_edge_l1norm_cost"
-                        )
-                        prog.AddLinearCost(np.sum(t))
-                        prog.AddLinearConstraint(
-                            A @ e_vars - t,
-                            np.ones(A.shape[0]) * (-np.inf),
-                            np.zeros(A.shape[0]),
-                        )
-                        prog.AddLinearConstraint(
-                            -A @ e_vars - t,
-                            np.ones(A.shape[0]) * (-np.inf),
-                            np.zeros(A.shape[0]),
-                        )
-                    else:
-                        prog.AddCost(cost, e_vars)
-
-            # Add variable for the total cost
-            prog.NewContinuousVariables(1, name="cost")
-
-            N = len(prog.decision_variables())
-
-        # logger.debug(
-        #     f"Total number of decision variables {N}, sum of v_dims {sum(v_dims)}"
-        # )
-        # Collect all the inequality and equality constraints
-        ASs, bs, CSs, ds = (
-            [np.empty((0, N))],
-            [np.empty((0))],
-            [np.empty((0, N))],
-            [np.empty((0))],
-        )
-        # A_i @ S_i will give the constraints for all the variables so can be vstacked to big_A
-        # (m x n) (n x N) = (m x N)
-
-        def process_constraint(constraint: Constraint, S_i):
-            # logger.debug(f"constraint {constraint}")
-            # logger.debug(f"lower bound {constraint.lower_bound()}")
-            # logger.debug(f"upper bound {constraint.upper_bound()}")
-            # Note: It is important that this branch comes first since LinearEqualityConstraint is a subclass of LinearConstraint
-            if isinstance(constraint, LinearEqualityConstraint):
-                # logger.debug(f"Adding linear equality constraint")
-                # Add to the equality constraints
-                CSs.append(constraint.GetDenseA() @ S_i)
-                # Note for equality constraints, the lower and upper bounds are the same
-                ds.append(constraint.lower_bound())
-                # logger.debug(f"Added to ds, {ds}")
-            elif isinstance(constraint, LinearConstraint):
-                # logger.debug(f"Adding linear constraint")
-                # Add to the inequality constraints
-                if not np.all(constraint.lower_bound() == -np.inf):
-                    # -Ax <= -b ==> Ax >= b
-                    ASs.append((-constraint.GetDenseA()) @ S_i)
-                    bs.append(-constraint.lower_bound())
-                if not np.all(constraint.upper_bound() == np.inf):
-                    ASs.append(constraint.GetDenseA() @ S_i)
-                    bs.append(constraint.upper_bound())
-            else:
-                raise ValueError(f"Unsupported constraint type {type(constraint)}")
-
-        # Process Vertex Constraints
-        for i, v_name in enumerate(node.vertex_path):
-            convex_set = self._graph.vertices[v_name].convex_set
-
-            S_i = create_selection_matrix(x[i], N)
-            # logger.debug(f"S_i.shape {S_i.shape}")
-            # Point In Set Constraints
-            if convex_set.A is not None and convex_set.A.shape[0] != 0:
-                ASs.append(convex_set.A @ S_i)
-                bs.append(convex_set.b)
-                # logger.debug(f"Point in set {i} added to bs, {bs}")
-            if convex_set.C is not None and convex_set.C.shape[0] != 0:
-                # logger.debug(f"C.shape {convex_set.C.shape}")
-                CSs.append(convex_set.C @ S_i)
-                ds.append(convex_set.d)
-                # logger.debug(f"Point in set {i} added to ds, {ds}")
-
-            # Vertex Constraints
-            # logger.debug(f"processing vertex constraint vertices[{i}]")
-            for binding in vertices[i].GetConstraints():
-                process_constraint(binding.evaluator(), S_i)
-
-        # Process Edge Constraints
-        for i, e in enumerate(edges):
-            # Edges will be two adjacent vertices in the path.
-            S_i = create_selection_matrix(x[i] + x[i + 1], N)
-            # Edge constraints
-            # logger.debug(f"processing edge constraint edge[{i}]")
-            for binding in e.GetConstraints():
-                process_constraint(binding.evaluator(), S_i)
-
-        if self.include_cost_epigraph:
-            # Process the extra constraints introduced by the costs
-            for binding in prog.GetAllConstraints():
-                con_var_indices = prog.FindDecisionVariableIndices(binding.variables())
-                S = create_selection_matrix(con_var_indices, N)
-                process_constraint(binding.evaluator(), S)
-
-            # Add the epigraph cost as the final row
-            cost_coeff_row = np.zeros((1, N))
-            # Assumes the cost variable is the last variable
-            cost_coeff_row[0, -1] = -1
-            cost_b = 0
-            for binding in prog.GetAllCosts():
-                cost = binding.evaluator()
-                if not isinstance(cost, LinearCost):
-                    raise NotImplementedError(
-                        f"Only linear costs are supported for now, {cost} not supported"
-                    )
-                cost_var_indices = prog.FindDecisionVariableIndices(binding.variables())
-                S = create_selection_matrix(cost_var_indices, N)
-                # Linear cost is of the form a^T x + b
-                # Transforming it to cost_coeff_row x <=  - cost_b
-                # (1, N) (N, 1) = scalar
-                # which then becomes: cost in terms of all other variables + cost_b <= cost_var
-                a_T = cost.a().reshape(1, -1)
-                cost_b += cost.b()
-                cost_coeff_row += a_T @ S
-
-            ASs.append(cost_coeff_row)
-            bs.append(np.array([-cost_b]))
-
-        # Stack all the ASs, bs, CSs, ds
-        big_A = np.vstack(ASs)
-        big_b = np.concatenate(bs)
-        big_C = np.vstack(CSs)
-        big_d = np.concatenate(ds)
-
-        return big_A, big_b, big_C, big_d
-
     def get_nullspace_path_mathematical_program(
         self, node: SearchNode, add_upper_bound=False, cost_upper_bound=500
     ) -> Tuple[MathematicalProgram, int]:
@@ -512,18 +309,17 @@ class AHContainmentDominationChecker(DominationChecker):
         full_v_dim = sum([v.ambient_dimension() for v in vertices])
 
         prog = MathematicalProgram()
+        # Name the vertices by index since cycles are allowed otherwise might get duplicate names.
         ns_vertex_vars = [
             (
-                prog.NewContinuousVariables(ns_set.dim, name=f"{v_name}_ns_vars")
+                prog.NewContinuousVariables(ns_set.dim, name=f"v{v_idx}_ns_vars")
                 if ns_set.dim > 0
                 else None
             )
-            for ns_set, v_name in zip(ns_sets, node.vertex_path)
+            for v_idx, ns_set in enumerate(ns_sets)
         ]
         ns_dim = sum([ns_set.dim for ns_set in ns_sets])
-        for v, v_name, ns_set, lam in zip(
-            vertices, node.vertex_path, ns_sets, ns_vertex_vars
-        ):
+        for idx, (v, ns_set, lam) in enumerate(zip(vertices, ns_sets, ns_vertex_vars)):
             # Handle the case where the affine subspace is a point
             if lam is None:
                 continue
@@ -532,7 +328,9 @@ class AHContainmentDominationChecker(DominationChecker):
 
             # Vertex Constraints
             for binding in v.GetConstraints():
-                raise NotImplementedError()
+                raise NotImplementedError(
+                    "Additional vertex constraints not supported yet"
+                )
                 constraint = binding.evaluator()
                 if not isinstance(constraint, LinearConstraint):
                     raise NotImplementedError(
@@ -548,11 +346,10 @@ class AHContainmentDominationChecker(DominationChecker):
                 for binding in v.GetCosts():
                     cost = binding.evaluator()
                     if isinstance(cost, L1NormCost):
-                        # logger.debug(f"Adding l1 norm cost for vertex {v_name}")
                         A = cost.A()
                         # logger.debug(f"A:\n{A}")
                         l = prog.NewContinuousVariables(
-                            A.shape[0], name=f"{v_name}_vertex_l1norm_cost"
+                            A.shape[0], name=f"v{idx}_vertex_l1norm_cost"
                         )
                         prog.AddLinearCost(np.ones(l.shape), l)
                         A_prime = np.hstack((A @ ns_set.V, -np.eye(A.shape[0])))
@@ -578,8 +375,8 @@ class AHContainmentDominationChecker(DominationChecker):
                     else:
                         raise NotImplementedError()
 
-        for i, (e, e_name) in enumerate(zip(edges, node.edge_path)):
-            # u, v = node.vertex_path[i], node.vertex_path[i + 1]
+        for i, e in enumerate(edges):
+            # u, v = ne_nameode.vertex_path[i], node.vertex_path[i + 1]
             u_vars, v_vars = ns_vertex_vars[i], ns_vertex_vars[i + 1]
             u_set, v_set = ns_sets[i], ns_sets[i + 1]
             # Both are points
@@ -753,10 +550,10 @@ class AHContainmentDominationChecker(DominationChecker):
 
         prog = MathematicalProgram()
         vertex_vars = [
-            prog.NewContinuousVariables(v.ambient_dimension(), name=f"{v_name}_vars")
-            for v, v_name in zip(vertices, node.vertex_path)
+            prog.NewContinuousVariables(v.ambient_dimension(), name=f"v{v_idx}_vars")
+            for v_idx, v in enumerate(vertices)
         ]
-        for v, v_name, x in zip(vertices, node.vertex_path, vertex_vars):
+        for v, x in zip(vertices, vertex_vars):
             v.set().AddPointInSetConstraints(prog, x)
 
             # Vertex Constraints
@@ -764,20 +561,14 @@ class AHContainmentDominationChecker(DominationChecker):
                 constraint = binding.evaluator()
                 prog.AddConstraint(constraint, x)
 
-        for e, e_name in zip(edges, node.edge_path):
+        for idx, e in enumerate(edges):
             # Edge Constraints
             for binding in e.GetConstraints():
                 constraint = binding.evaluator()
                 variables = binding.variables()
-                u_name, v_name = (
-                    self._graph.edges[e_name].u,
-                    self._graph.edges[e_name].v,
-                )
-                u_idx, v_idx = node.vertex_path.index(u_name), node.vertex_path.index(
-                    v_name
-                )
-                variables[: len(vertex_vars[u_idx])] = vertex_vars[u_idx]
-                variables[-len(vertex_vars[v_idx]) :] = vertex_vars[v_idx]
+
+                u_idx, v_idx = idx, idx + 1
+                variables = np.hstack((vertex_vars[u_idx], vertex_vars[v_idx]))
                 prog.AddConstraint(constraint, variables)
 
         return prog
@@ -790,10 +581,10 @@ class AHContainmentDominationChecker(DominationChecker):
         prog = MathematicalProgram()
 
         vertex_vars = [
-            prog.NewContinuousVariables(v.ambient_dimension(), name=f"{v_name}_vars")
-            for v, v_name in zip(vertices, node.vertex_path)
+            prog.NewContinuousVariables(v.ambient_dimension(), name=f"v{v_idx}_vars")
+            for v_idx, v in enumerate(vertices)
         ]
-        for v, v_name, x in zip(vertices, node.vertex_path, vertex_vars):
+        for idx, (v, x) in enumerate(zip(vertices, vertex_vars)):
             v.set().AddPointInSetConstraints(prog, x)
 
             # Vertex Constraints
@@ -807,7 +598,7 @@ class AHContainmentDominationChecker(DominationChecker):
                 if isinstance(cost, L1NormCost):
                     A = cost.A()
                     t = prog.NewContinuousVariables(
-                        A.shape[0], name=f"{v_name}_vertex_l1norm_cost"
+                        A.shape[0], name=f"v{idx}_vertex_l1norm_cost"
                     )
                     prog.AddLinearCost(np.sum(t))
                     prog.AddLinearConstraint(
@@ -821,30 +612,21 @@ class AHContainmentDominationChecker(DominationChecker):
                 else:
                     prog.AddCost(cost, x)
 
-        for e, e_name in zip(edges, node.edge_path):
-            u_name, v_name = self._graph.edges[e_name].u, self._graph.edges[e_name].v
-            u_idx, v_idx = node.vertex_path.index(u_name), node.vertex_path.index(
-                v_name
-            )
+        for idx, e in enumerate(edges):
+            u_idx, v_idx = idx, idx + 1
+            variables = np.hstack((vertex_vars[u_idx], vertex_vars[v_idx]))
             # Edge Constraints
             for binding in e.GetConstraints():
                 constraint = binding.evaluator()
-                variables = binding.variables()
-
-                variables[: len(vertex_vars[u_idx])] = vertex_vars[u_idx]
-                variables[-len(vertex_vars[v_idx]) :] = vertex_vars[v_idx]
                 prog.AddConstraint(constraint, variables)
 
             # Edge Costs
             for binding in e.GetCosts():
                 cost = binding.evaluator()
-                variables = binding.variables()
-                variables[: len(vertex_vars[u_idx])] = vertex_vars[u_idx]
-                variables[-len(vertex_vars[v_idx]) :] = vertex_vars[v_idx]
                 if isinstance(cost, L1NormCost):
                     A = cost.A()
                     t = prog.NewContinuousVariables(
-                        A.shape[0], name=f"{e_name}_edge_l1norm_cost"
+                        A.shape[0], name=f"e{idx}_edge_l1norm_cost"
                     )
                     prog.AddLinearCost(np.sum(t))
                     prog.AddLinearConstraint(
@@ -859,7 +641,6 @@ class AHContainmentDominationChecker(DominationChecker):
                     )
                 else:
                     prog.AddCost(cost, variables)
-
         return prog
 
     def get_feasibility_matrices_via_prog(self, node: SearchNode):
